05a8bebca99d211819f1f9b679cdc0ef8c91250e
edwinbrower <edwin.brower@gmail.com>
2017-03-03 19:20:20 -0800

    Add chatterbox client files into client folder

--- server/models/index.js
+++ server/models/index.js
@@ -13,3 +13,6 @@ module.exports = {
   }
 };
 
+// server/models/index.js defines the messages and users models that 
+// your application will use. Skeletons of the models have already 
+// been created but you'll have to write out the details for their methods
